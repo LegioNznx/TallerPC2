de7024ef761836deec6f1a5484a9c375296680a3
Jean Franco <112286911+LegioNznx@users.noreply.github.com>
2025-11-21 21:47:33 -0500

    Update Producto.java

--- junit-taller/src/main/java/modelo/Producto.java
+++ junit-taller/src/main/java/modelo/Producto.java
@@ -1,15 +1,36 @@
 package modelo;
 
+import java.util.Objects;
+
 public class Producto {
+
     private String nombre;
     private double precio;
     private int cantidad;
+    private String sku;
+    private String categoria;
+    private boolean esActivo;
+    private boolean descuentoAplicable;
+
+
+    public Producto(String nombre, double precio, int cantidad,
+                    String sku, String categoria,
+                    boolean esActivo, boolean descuentoAplicable) {
+        this.nombre = nombre;
+        setPrecio(precio);       
+        setCantidad(cantidad);   
+        this.sku = sku;
+        this.categoria = categoria;
+        this.esActivo = esActivo;
+        this.descuentoAplicable = descuentoAplicable;
+    }
+
 
     public Producto(String nombre, double precio, int cantidad) {
-        this.nombre = nombre;
-        this.precio = precio;
-        this.cantidad = cantidad;
+        this(nombre, precio, cantidad, null, null, true, false);
     }
+
+    
 
     public String getNombre() {
         return nombre;
@@ -23,7 +44,11 @@ public class Producto {
         return precio;
     }
 
+   
     public void setPrecio(double precio) {
+        if (precio < 0) {
+            throw new IllegalArgumentException("El precio no puede ser negativo");
+        }
         this.precio = precio;
     }
 
@@ -31,16 +56,73 @@ public class Producto {
         return cantidad;
     }
 
+   
     public void setCantidad(int cantidad) {
+        if (cantidad < 0) {
+            throw new IllegalArgumentException("La cantidad no puede ser negativa");
+        }
         this.cantidad = cantidad;
     }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public boolean isEsActivo() {
+        return esActivo;
+    }
+
+    public void setEsActivo(boolean esActivo) {
+        this.esActivo = esActivo;
+    }
+
+    public boolean isDescuentoAplicable() {
+        return descuentoAplicable;
+    }
+
+    public void setDescuentoAplicable(boolean descuentoAplicable) {
+        this.descuentoAplicable = descuentoAplicable;
+    }
+
+   
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Producto)) return false;
+        Producto producto = (Producto) o;
+        return Objects.equals(sku, producto.sku); 
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sku);
+    }
+
+    
 
     @Override
     public String toString() {
         return "Producto{" +
                 "nombre='" + nombre + '\'' +
+                ", sku='" + sku + '\'' +
+                ", categoria='" + categoria + '\'' +
                 ", precio=" + precio +
                 ", cantidad=" + cantidad +
+                ", esActivo=" + esActivo +
+                ", descuentoAplicable=" + descuentoAplicable +
                 '}';
     }
 }
